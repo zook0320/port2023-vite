ebc6c0bc3e54ca39bfa4b0fd7156dad880682181
zook0320 <zook020320@gmail.com>
2023-11-05 13:10:21 +0900

    레이아웃 완성

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<div id="skip">
+    <a href="#header">헤더 영역 바로가기</a>
+    <a href="#intro">소개 영역 바로가기</a>
+    <a href="#skill">스킬 영역 바로가기</a>
+    <a href="#site">사이트 영역 바로가기</a>
+    <a href="#port">포트폴리오 영역 바로가기</a>
+    <a href="#contact">연락처 영역 바로가기</a>
+    <a href="#footer">푸터 영역 바로가기</a>
+</div>
+<main id="main" role="main">
+    <section id="intro">
+        intro
+    </section>
+
+
+    <section id="skill">
+        skill
+    </section> 
+
+
+    <section id="site">
+        site
+    </section>
+
+
+    <section id="port">
+        port
+    </section>
+
+
+    <section id="contact">
+        contact
+    </section>
+
+</main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { menu } from "./section/menu.js";
+import { port } from "./section/port";
+import { link } from "./section/link.js";
+import { smooth } from "./section/smooth.js";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+window.addEventListener("load", function () {
+    smooth();
+    link();
+    menu();
+    port();
+});
